5a7cb3170728f3a08f5b186c920830c5d7ed1290
habipwalue <habip.okcu@walue.ws>
2025-05-02 12:12:09 +0300

    fix: Vercel adapter middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,7 @@
 // src/middleware.ts
 import { defineMiddleware } from 'astro:middleware';
 
-const blockedIPs: string[] = [
-    '188.58.71.17', // Engellemek istediğiniz IP adres(ler)i
-];
-
-// 'server' modunda bu her zaman sunucu/edge'de çalışacak
 export const onRequest = defineMiddleware((context, next) => {
-    const ip = context.clientAddress;
-    // console.log(`[Server] Gelen istek IP: ${ip}`);
-
-    if (ip && blockedIPs.includes(ip)) {
-        console.log(`[Server] Engellenen IP (${ip}) erişimi engellendi.`);
-        return new Response('Erişim Engellendi (Access Denied)', {
-            status: 403
-        });
-    }
-
-    return next();
+  console.log("Middleware çalıştı, IP kontrolü devredışı."); // Log ekleyelim
+  return next(); // Sadece sonraki adıma geç
 });
