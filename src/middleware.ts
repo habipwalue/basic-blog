66e88ba973ae591a46bc74ac368095f12bd78a8d
habipwalue <habip.okcu@walue.ws>
2025-05-02 14:54:56 +0300

    chore: Vercel build'ini tetiklemek için küçük değişiklik ve ayar kontrolü1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,16 @@
 // src/middleware.ts
-import { defineMiddleware } from 'astro:middleware';
+import type { MiddlewareHandler } from 'astro';
 
-export const onRequest = defineMiddleware((context, next) => {
-  console.log("Middleware çalıştı, IP kontrolü devredışı."); // Log ekleyelim
-  console.log("Middleware yeniden çalıştı, build testi.");
-  return next(); // Sadece sonraki adıma geç
-});
+export const onRequest: MiddlewareHandler = async ({ request }, next) => {
+  // X-Forwarded-For en öndeki IP’yi içerir
+  const ip = request.headers.get('x-forwarded-for')?.split(',')[0] || '';
+  
+  // Engellenmek istenen IP listesi
+  const blocked = ['188.58.71.17'];
+  
+  if (blocked.includes(ip)) {
+    return new Response('Erişim Engellendi', { status: 403 });
+  }
+  
+  return next();
+};
